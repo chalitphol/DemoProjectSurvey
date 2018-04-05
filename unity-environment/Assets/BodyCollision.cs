b5bf44143fd052fa37860214c28292713e3ce583
chalitphol kuensuwan <chalitphol@gmail.com>
2018-04-06 02:38:52 +0700

    1st commit

---
+++ unity-environment/Assets/BodyCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyCollision : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("wall"))
+        {
+            print(MLCarAgent.checkbool);
+            MLCarAgent.checkbool = true;
+        }
+    }
+}
